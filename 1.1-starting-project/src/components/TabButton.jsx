67969f671092ac80f6f5f2d64236244924b13722
Oleksandr Vlasov <drumlife182@gmail.com>
2024-11-24 17:54:36 +0100

    Worked with reacting to Event

--- 1.1-starting-project/src/components/TabButton.jsx
+++ 1.1-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,11 @@
 export default function TabButton({ children }) {
+  function handleClick() {
+    console.log("Handle click is working");
+  }
+
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
