8edd493b6b06c9dbd423774e406bc966bee85539
Oleksandr Vlasov <drumlife182@gmail.com>
2024-11-25 17:35:02 +0100

    Passing Functions as Values to Props

--- 1.1-starting-project/src/App.jsx
+++ 1.1-starting-project/src/App.jsx
@@ -7,6 +7,10 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log("Handle click is working - selected");
+  }
+
   return (
     <div>
       <Header />
@@ -23,11 +27,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
