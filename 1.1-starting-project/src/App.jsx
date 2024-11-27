5cbedfa9a0894d7444627a0e379f9aec9572c3f9
Oleksandr Vlasov <drumlife182@gmail.com>
2024-11-27 09:24:32 +0100

    Managing State & Using Hooks[Core Concept]

--- 1.1-starting-project/src/App.jsx
+++ 1.1-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 
 import Header from "./components/Header/Header.jsx";
@@ -7,11 +7,12 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  let tabContent = "Please click a button";
+  const [selectedTopic, setSelectedTopic] = useState(
+    "Please click a tab button"
+  );
 
   function handleSelect(selectedButton) {
-    tabContent = selectedButton;
-    console.log(tabContent);
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -37,7 +38,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {tabContent}
+          {selectedTopic}
         </section>
       </main>
     </div>
