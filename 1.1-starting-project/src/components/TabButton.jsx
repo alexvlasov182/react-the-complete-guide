0b5b6ad84b7caf82f87122387ca463875154fcf8
Oleksandr Vlasov <drumlife182@gmail.com>
2024-12-01 20:47:45 +0100

    Forwarding Props To Wrapped Elements

--- 1.1-starting-project/src/components/TabButton.jsx
+++ 1.1-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,9 @@
-export default function TabButton({ children, onSelect, isSelected }) {
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/prop-types */
+export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+      <button className={isSelected ? "active" : undefined} {...props}>
         {children}
       </button>
     </li>
