3c497fa88aa6748807682a3ce2ef48cb630a4213
Oleksandr Vlasov <drumlife182@gmail.com>
2024-12-05 18:07:53 +0100

    First Steps Towards Our Tic-Tac-Toe Game

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,6 +4,19 @@ function App() {
   return (
     <>
       <Header />
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
     </>
   );
 }
