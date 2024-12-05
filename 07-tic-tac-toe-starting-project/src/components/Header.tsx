c5e6405c320395a8504d9fabc5618b1a09b9f9d0
Oleksandr Vlasov <drumlife182@gmail.com>
2024-12-05 17:57:23 +0100

    Started project

---
+++ 07-tic-tac-toe-starting-project/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <img src="../../public/game-logo.png" alt="game log" />
+      <h1>Tic-Tac-Toe</h1>
+    </>
+  );
+}
