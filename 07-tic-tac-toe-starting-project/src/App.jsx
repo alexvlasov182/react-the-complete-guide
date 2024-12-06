345ebd1a7f67ee0e1922b5808bb10edd0581e88d
Oleksandr Vlasov <drumlife182@gmail.com>
2024-12-06 16:55:59 +0100

    Concept Repetition: Splitting Components & Building Resusable Components

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Player from "./components/Player";
 
 function App() {
   return (
@@ -6,14 +7,8 @@ function App() {
       <Header />
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">O</span>
-          </li>
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
         </ol>
         Game Board
       </div>
