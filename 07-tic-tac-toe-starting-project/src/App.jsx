caa93fb3450933e707244cd6a3c0ba622143ce44
Oleksandr Vlasov <drumlife182@gmail.com>
2024-12-08 12:23:32 +0100

    Rendering Multi-Dimensional Lists

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,18 +1,20 @@
+import GameBoard from "./components/GameBoard";
 import Header from "./components/Header";
 import Player from "./components/Player";
 
 function App() {
   return (
-    <>
+    <main>
       <Header />
       <div id="game-container">
         <ol id="players">
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
-        Game Board
+        <GameBoard />
       </div>
-    </>
+      LOG
+    </main>
   );
 }
 
