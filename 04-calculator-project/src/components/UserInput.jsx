3026bca985dd02b1a3f6466bd27ba44982909543
Oleksandr Vlasov <drumlife182@gmail.com>
2024-12-23 19:50:53 +0100

    Commit

--- 04-calculator-project/src/components/UserInput.jsx
+++ 04-calculator-project/src/components/UserInput.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 export default function UserInput({ onChange, userInput }) {
+  console.log("log for commit");
+
   return (
     <section id="user-input">
       <div className="input-group">
