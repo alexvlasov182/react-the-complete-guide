1ad1c683df48e6f9fdabc8995da5743199505246
Oleksandr Vlasov <drumlife182@gmail.com>
2025-01-07 07:13:16 +0100

    Managing Other Values with Refs

--- 06-working-with-refs-portals/src/components/TimerChallenge.jsx
+++ 06-working-with-refs-portals/src/components/TimerChallenge.jsx
@@ -1,8 +1,6 @@
 import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
-  console.log("TimerChallenge render");
-
   const timer = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
