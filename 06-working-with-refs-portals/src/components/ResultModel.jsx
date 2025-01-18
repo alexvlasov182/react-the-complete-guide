d4cb0fe9aea36856c1bb3b02f7ece36749dbfdb5
Oleksandr Vlasov <drumlife182@gmail.com>
2025-01-18 08:58:29 +0100

    Enhancing the Demo App "Result Modal"

--- 06-working-with-refs-portals/src/components/ResultModel.jsx
+++ 06-working-with-refs-portals/src/components/ResultModel.jsx
@@ -11,6 +11,7 @@ export default function ResultModel({
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -23,6 +24,7 @@ export default function ResultModel({
   return (
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>Your lost</h2>}
+      {!userLost && <h2>You won: your score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
