270232d4cb41497160eabce3cd8479cf6ef2fd96
Oleksandr Vlasov <drumlife182@gmail.com>
2025-02-01 09:28:40 +0100

    Prague - Home(Munich)

--- 07-project-management/src/App.jsx
+++ 07-project-management/src/App.jsx
@@ -3,6 +3,7 @@ import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  console.log("Prague - Home");
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
