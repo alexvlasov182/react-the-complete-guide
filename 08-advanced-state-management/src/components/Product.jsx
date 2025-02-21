405a4ed5e6e337cded6e39153908d48322c6ed4c
Oleksandr Vlasov <drumlife182@gmail.com>
2025-02-21 07:44:14 +0100

    Context

--- 08-advanced-state-management/src/components/Product.jsx
+++ 08-advanced-state-management/src/components/Product.jsx
@@ -8,6 +8,8 @@ export default function Product({
   description,
   onAddToCart,
 }) {
+  console.log("Working with context");
+
   return (
     <article className="product">
       <img src={image} alt={title} />
