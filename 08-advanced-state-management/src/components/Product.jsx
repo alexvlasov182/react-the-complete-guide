3ecece0b421836b0d1153245c87a850a94ba3da7
Oleksandr Vlasov <drumlife182@gmail.com>
2025-02-26 18:05:34 +0100

    Checked image

--- 08-advanced-state-management/src/components/Product.jsx
+++ 08-advanced-state-management/src/components/Product.jsx
@@ -8,7 +8,7 @@ export default function Product({
   description,
   onAddToCart,
 }) {
-  console.log("id", id);
+  console.log("image", image);
   return (
     <article className="product">
       <img src={image} alt={title} />
