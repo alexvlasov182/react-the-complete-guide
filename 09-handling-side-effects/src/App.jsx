25fe4eac354fd01bc1accc57c8463b9361e32e4e
Oleksandr Vlasov <drumlife182@gmail.com>
2025-08-05 18:23:58 +0200

    docs: not all side effects need useEffect

--- 09-handling-side-effects/src/App.jsx
+++ 09-handling-side-effects/src/App.jsx
@@ -41,6 +41,14 @@ function App() {
       const place = AVAILABLE_PLACES.find(place => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        'selectedPlaces',
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -77,7 +85,7 @@ function App() {
         <Places
           title="Available Places"
           places={availavelPlaces}
-          fallbackText="Sorting places by distances..."
+          fallbackText="Sorting places by distances...c"
           onSelectPlace={handleSelectPlace}
         />
       </main>
